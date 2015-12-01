723b7fd23a08b9b604b1f3fadfcfd17605e270b3
Malte Stær Nissen <nissen@di.ku.dk>
2015-12-01 15:16:23 +0100

    protobuf .cc and .h generator

---
+++ scripts/GeneratePB.bat
@@ -0,0 +1,6 @@
+if exist "../src/caffe/proto/caffe.pb.h" (
+    echo caffe.pb.h remains the same as before
+) else (
+    echo caffe.pb.h is being generated
+    "C:/caffe-dependencies/protobuf/bin/protoc" -I="../src/caffe/proto" --cpp_out="../src/caffe/proto" "../src/caffe/proto/caffe.proto"
+)
