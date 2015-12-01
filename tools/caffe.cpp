deb5a3fd48312663eb33891ba432fd0be4b4201a
Malte Stær Nissen <nissen@di.ku.dk>
2015-12-01 13:57:30 +0100

    Signal handler out-commented

--- tools/caffe.cpp
+++ tools/caffe.cpp
@@ -12,7 +12,7 @@ namespace bp = boost::python;
 
 #include "boost/algorithm/string.hpp"
 #include "caffe/caffe.hpp"
-#include "caffe/util/signal_handler.h"
+//#include "caffe/util/signal_handler.h"
 
 using caffe::Blob;
 using caffe::Caffe;
@@ -188,16 +188,16 @@ int train() {
     Caffe::set_mode(Caffe::GPU);
     Caffe::set_solver_count(gpus.size());
   }
-
+  /*
   caffe::SignalHandler signal_handler(
         GetRequestedAction(FLAGS_sigint_effect),
         GetRequestedAction(FLAGS_sighup_effect));
-
+  */
   shared_ptr<caffe::Solver<float> >
       solver(caffe::SolverRegistry<float>::CreateSolver(solver_param));
-
+  /*
   solver->SetActionFunction(signal_handler.GetActionFunction());
-
+  */
   if (FLAGS_snapshot.size()) {
     LOG(INFO) << "Resuming from " << FLAGS_snapshot;
     solver->Restore(FLAGS_snapshot.c_str());
