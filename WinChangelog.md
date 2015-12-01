a45c819e7c2fc65b85258fbb77e51af2e74411c7
Malte Stær Nissen <nissen@di.ku.dk>
2015-12-01 14:29:41 +0100

    Various windows ports

---
+++ WinChangelog.md
@@ -0,0 +1,4 @@
+Changelog:
+
+- Signal handler not supported (training cannot take snapshots)
+- Various ports using defines and ifdef / ifndef
--- src/caffe/solver.cpp
+++ src/caffe/solver.cpp
@@ -8,7 +8,10 @@
 #include "caffe/util/hdf5.hpp"
 #include "caffe/util/io.hpp"
 #include "caffe/util/upgrade_proto.hpp"
-
+// port for Win32
+#ifdef _MSC_VER
+  #define snprintf sprintf_s
+#endif
 namespace caffe {
 
 template<typename Dtype>
