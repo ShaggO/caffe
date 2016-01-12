b7aeff5b02ef828053d32e40ad5b113397934227
Malte Stær Nissen <nissen@di.ku.dk>
2016-01-12 16:43:22 +0100

    Fixed size check for label masks

--- src/caffe/layers/accuracy_layer.cpp
+++ src/caffe/layers/accuracy_layer.cpp
@@ -28,7 +28,7 @@ void AccuracyLayer<Dtype>::Reshape(
       bottom[0]->CanonicalAxisIndex(this->layer_param_.accuracy_param().axis());
   outer_num_ = bottom[0]->count(0, label_axis_);
   inner_num_ = bottom[0]->count(label_axis_ + 1);
-  CHECK_EQ(outer_num_ * inner_num_, bottom[1]->count())
+  CHECK_EQ(outer_num_ * inner_num_, bottom[1]->count(0, label_axis_) * bottom[1]->count(label_axis_ + 1))
       << "Number of labels must match number of predictions; "
       << "e.g., if label axis == 1 and prediction shape is (N, C, H, W), "
       << "label count (number of labels) must be N*H*W, "
