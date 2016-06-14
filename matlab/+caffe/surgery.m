15dcfde5f0c603e904a834807c9e0d39b025b23c
nissen @ gpu cluster <nissen@di.ku.dk>
2016-06-14 15:31:12 +0200

    ip2conv now copies all compatible layer parameters even when given an initial mapping of layers

--- matlab/+caffe/surgery.m
+++ matlab/+caffe/surgery.m
@@ -11,22 +11,24 @@ classdef surgery
       % Enumerate the names of the IP layers and Conv layers in
       % inputs
       if nargin == 2
-          % No layers specified. Simply convert all layers with
-          % same names and non-matching dimensions
           layersIp = {};
-          for name = netIp.layer_names'
-              layer = netIp.layers(char(name));
-              if numel(layer.params) > 0
-                  layer2 = netConv.layers(char(name));
-                  for i = 1:numel(layer.params)
-                      if prod(layer.params(i).shape) ==...
-                              prod(layer2.params(i).shape)
-                          layersIp = [layersIp,name];
-                      end
+          layersConv = {};
+      end
+
+      % Generate mapping lists. All layers with matching names and number of parameters
+      % are added to the current mapping
+      for name = netIp.layer_names'
+          layer = netIp.layers(char(name));
+          if numel(layer.params) > 0 && any(strcmp(netConv.layer_names,char(name)))
+              layer2 = netConv.layers(char(name));
+              for i = 1:numel(layer.params)
+                  if prod(layer.params(i).shape) ==...
+                          prod(layer2.params(i).shape)
+                      layersIp = [layersIp,name];
+                      layersConv = [layersConv,name];
                   end
               end
           end
-          layersConv = layersIp;
       end
       assert(numel(layersIp) == numel(layersConv),'Number of original and new layers should be equal');
       for i = 1:numel(layersIp)
